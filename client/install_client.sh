8b13263caaee23c55e911d2ab72784d30aabd39d
sangyeon kim <greyhairchooselife@gmail.com>
2023-11-28 18:06:50 +0900

    choose node version for client-side setup
    Add user interaction for choosing just latest or specific version of
    node which is for docker containers.

--- client/install_client.sh
+++ client/install_client.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+echo -e "\n## Start config for CLIENT-SIDE ##\n"
+
 mkdir -p $project_name/client/configs
 
 touch $project_name/client/configs/.env.dev \
       $project_name/client/configs/.env \
 
+# Add user interaction for choosing node version for docker containers
+docker_container_node_version=latest
+read -p 'Try latest version of node for docker container? (y/n): ' use_latest
+
+if [ "$use_latest" = "n" ]; then
+  read -e -p 'Which version of node? : ' docker_container_node_version_input
+  eval docker_container_node_version='$docker_container_node_version_input'
+fi
+
 # Write client/dev.Dockerfile
 cat << EOF > $project_name/client/dev.Dockerfile
-FROM node:14-alpine
+FROM "node:$docker_container_node_version"
 
 WORKDIR /app
 
@@ -22,7 +33,7 @@ EOF
 
 # Write client/Dockerfile
 cat << EOF > $project_name/client/Dockerfile
-FROM node:14-alpine
+FROM "node:$docker_container_node_version"
 
 WORKDIR /app
 
@@ -32,3 +43,5 @@ RUN npm install -g serve
 
 CMD serve -s build -l 3000
 EOF
+
+echo -e "\n... Done!\n"
